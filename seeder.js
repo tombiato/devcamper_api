6f85a9bc6ceff6d210b06cf5d38f07c6a25716ad
Tom Biato <tom.biato@ynov.com>
2021-03-12 10:12:01 +0100

    Course Model & Seeding

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,10 +23,13 @@ const bootcamps = JSON.parse(
 	fs.readFileSync(`${__dirname}/_data/bootcamps.json`)
 );
 
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`));
+
 // Import into DB
 const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps);
+		await Course.create(courses);
 
 		console.log('Date Imported...'.green.inverse);
 		process.exit();
@@ -38,6 +42,7 @@ const importData = async () => {
 const deleteData = async () => {
 	try {
 		await Bootcamp.deleteMany();
+		await Course.deleteMany();
 
 		console.log('Date Destoyed...'.red.inverse);
 		process.exit();
