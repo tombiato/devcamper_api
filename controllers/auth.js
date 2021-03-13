465ab0cb59001d1820f7e1eddcb75abb50df7a42
Tom Biato <tom.biato@ynov.com>
2021-03-13 16:49:10 +0100

    Auth Protect Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -63,3 +63,12 @@ const sendTokenResponse = (user, statusCode, res) => {
 		.cookie('token', token, options)
 		.json({ success: true, token });
 };
+
+// @desc      Get surrent logged in user
+// @route     POST /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.user.id);
+
+	res.status(200).json({ success: true, data: user });
+});
