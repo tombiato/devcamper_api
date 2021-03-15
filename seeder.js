02f7bff9616c1faad9bfae80c2d1a6f71dde67cc
Tom Biato <tom.biato@ynov.com>
2021-03-15 17:52:02 +0100

    Get Single Review & Update Seeder

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -28,12 +29,15 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`));
 
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`));
 
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`));
+
 // Import into DB
 const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps);
 		await Course.create(courses);
 		await User.create(users);
+		await Review.create(reviews);
 
 		console.log('Date Imported...'.green.inverse);
 		process.exit();
@@ -48,6 +52,7 @@ const deleteData = async () => {
 		await Bootcamp.deleteMany();
 		await Course.deleteMany();
 		await User.deleteMany();
+		await Review.deleteMany();
 
 		console.log('Date Destoyed...'.red.inverse);
 		process.exit();
